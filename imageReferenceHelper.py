f14ef63c3d230b9ab1daa7865e0c10d3b00d5523
PotatoNato <93164459+realPotatoNato@users.noreply.github.com>
2022-10-08 17:41:56 -0400

    allowed author to be added: lol

--- imageReferenceHelper.py
+++ imageReferenceHelper.py
@@ -1,12 +1,36 @@
 import pyperclip
 
 def main():
-    descType = input('Description of image: ')
-    theLink = input('Copy and paste website link: ')
     
-    finalOutput = f'[{descType}]. Retrieved from {theLink}'
+    t = input('Who is the author(Last name, first inital, second inital): ')
+    if t != '':
+        author = t
+    else:
+        author = ''
+    t = input('Title of the image: ')
+    if t != '':
+        title = t + ' '
+    else:
+        title = ''
+    descType = input('Type of image (NEEDED): ')
+    t = input('Year created: ')
+    if t != '':
+        year = f'({t}). '
+    else:
+        year = ''
+    theLink = input('Copy and paste website link (NEEDED): ')
+    
+    if author != '':
+        finalOutput = f'{author}. {year}{title}[{descType}]. Retrieved from {theLink}'
+    else:
+        finalOutput = f'{title}[{descType}]. {year}Retrieved from {theLink}'
     
     print(finalOutput)
     pyperclip.copy(finalOutput)
-    print("Output copied!")
+    redo = input("Output copied, y to make another reference, n to quit: ")
+    if redo == 'y':
+        main()
+    else:
+        print("Bye!")
+    
 main()
